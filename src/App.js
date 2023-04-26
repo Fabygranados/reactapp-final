5037b1650066767e4d7b89436a84b07ba7cc06a9
Fabiola Granados <faby.granados@gmail.com>
2023-04-26 16:39:36 -0700

    Added repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
+    <h1> Weather App</h1>
+     <footer> This is project was coded by ✨Faby Granados and is {" "}
+     <a href="https://github.com/Fabygranados/reactapp-final" target="_blank">
+      Open-Sourced on GitHub
+     </a>
+    </footer>
     </div>
   );
 }
