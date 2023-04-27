18ae8c8cf7734d5804d5b1e164ab1f78de831d5f
Fabiola Granados <faby.granados@gmail.com>
2023-04-26 20:01:34 -0700

    add bootstrap and update

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    <h1> Weather App</h1>
-     <footer> This is project was coded by ✨Faby Granados and is {" "}
-     <a href="https://github.com/Fabygranados/reactapp-final">
+      <div className="container">
+      <h1> Weather App React</h1>
+      <footer> This is project was coded by ✨Faby Granados and is {" "}
+       <a href="https://github.com/Fabygranados/reactapp-final">
       Open-Sourced on GitHub
-     </a>
-    </footer>
+      </a>
+      </footer>
+     </div>
     </div>
   );
 }
